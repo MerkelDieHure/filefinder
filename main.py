233c8c7ddbf692a54126b319e6d1cce78a0fe6cb
Cracklutscher <info@verfassungsschutz.net>
2023-05-29 12:53:53 +0200

    This is my Project initial state.

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+def search_files(directory, extension):
+    foundFiles = []
+    for item in os.listdir(directory):
+        itemPath = os.path.join(directory, item)
+        if os.path.isdir(itemPath):
+            foundFiles.extend(search_files(itemPath, extension))
+        elif os.path.isfile(itemPath) and item.endswith(extension):
+            foundFiles.append(itemPath)
+    return rem_dir(foundFiles)
+
+def rem_dir(foundFiles):
+    splitFiles = []
+    for element in foundFiles:
+        if isinstance(element, str):
+            newElement = element.split(directory,1)
+            splitFiles.append(newElement)
+            print(newElement)
+        else:
+            splitFiles.append(element)
+    return splitFiles
+
+
+directory = 'C:\\Users\jan\Desktop\\testus\\'
+extension = '.pdf'
+foundFiles = search_files(directory, extension)
+
+print(foundFiles)
+print(len(foundFiles))
